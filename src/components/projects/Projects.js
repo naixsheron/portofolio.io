5508b8cc7e8c3c3e09fbb6f1cdffae2c43861ec0
naixsheron <gaborflorinnicolae@yahoo.com>
2023-06-10 12:46:31 +0300

    +proiect

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import proiect1 from "../../assets/proiect1.jpg";
+import piata from "../../assets/piata.jpg";
 import bee from "../../assets/bee.jpg";
 import Title from "../home/Title";
 import ProjectCard from "./ProjectCard";
@@ -10,7 +11,7 @@ const Projects = () => {
       <Title title="Recent" subTitle="Projects" />
       <div className="w-full grid grid-cols-2 max-sm:grid-cols-1 gap-10">
         <div className="px-6">
-          <a href="https://gilded-cassata-988dcf.netlify.app/">
+          <a href="https://gilded-cassata-988dcf.netlify.app/" target="_blank">
             <ProjectCard
               title="React App"
               category="E-commerce"
@@ -19,8 +20,20 @@ const Projects = () => {
           </a>
         </div>
         <div className="px-6">
-          <a href="https://albinadeaur1.onrender.com/">
+          <a href="https://albinadeaur1.onrender.com/" target="_blank">
             <ProjectCard title="MERN App" category="E-commerce" image={bee} />
+          </a>
+        </div>
+        <div className="px-6">
+          <a
+            href="https://brilliant-longma-b4a5cb.netlify.app/"
+            target="_blank"
+          >
+            <ProjectCard
+              title="Small React App"
+              category="E-commerce"
+              image={piata}
+            />
           </a>
         </div>
         <div className="px-6"></div>
